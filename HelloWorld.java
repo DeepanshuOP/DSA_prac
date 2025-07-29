318cf3eae7da843e0f0da948c78f5f409e70db1c
Deepanshu <deepanshuop@gmail.com>
2025-07-30 00:26:24 +0530

    Java Basics

---
+++ HelloWorld.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        /*System.out .println("Enter the Name:-");
+        String name=input.nextLine();
+
+        System.out.println("Enter your Age:- ");
+        int age=input.nextInt();
+
+        System.out.println("Your name is "+name+" and Your age is "+age+" .");
+        if(age>=20){
+            System.out.println("You are Old.");
+        }
+        else{
+            System.out.println("You are Young.");
+        }
+        */
+        /*int i=1;
+        while(i<=10){
+            System.out.println("You are Still Here."+i);
+            i=i+1;
+        }
+        int sum=0;
+        for(int j=1;j<=10;j++){
+            sum=sum + i;
+        }
+        System.out.println("Sum is: "+sum);
+
+        System.out.println("Enter Temeperature in Celcius:");
+        float c= input.nextFloat();
+
+        float f= (c*9/5)+32;
+        System.out.println("The Temperature in Farahnheit: "+f);
+        */
+        /*int  i =1;
+        do{
+            System.out.println(i);
+            i=i+1;
+        }while(i<0);
+        */
+        input.close();
+    }
+}
